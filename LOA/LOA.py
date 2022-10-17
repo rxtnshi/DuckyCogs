5cd6ada8fec5d21f199dc9b76222f828c42fc776
Insurgent <rli60609@gmail.com>
2022-10-17 18:43:57 -0500

    Added LOA stuff
    yes

--- LOA/LOA.py
+++ LOA/LOA.py
@@ -1,5 +1,4 @@
 from redbot.core import commands
-from discord.ext.commands import cooldown, BucketType
 
 import discord
 
@@ -9,13 +8,10 @@ class LOA(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
        
-   @commands.command()
-   @commands.has_role("Staff")
-   async def loa(invoke_without_command=True) {
-    """LOA form"""
-    embed = discord.Embed(title="Leave of Absence Request", description="The LOA cog has been replaced with a form for easier tracking.", color=0xFF0000)
-        embed.add_field(name="To request a leave, please click here.", value="Make sure to fill out the required information in the form in order to request a leave.", url="https://google.com")
+    @commands.group(invoke_without_command=True)
+    @commands.has_role("Staff")
+    async def loa(self, ctx):
+        """LOA form"""
+        embed = discord.Embed(title="Leave of Absence Request", description="The LOA cog has been replaced with a form for easier tracking.", color=0xFF0000, url="https://forms.gle/yynHeRZVYqaohGj77")
+        embed.add_field(name="To request a leave, please click the title", value="Make sure to fill out the required information in the form in order to request a leave.")
         await ctx.send(embed=embed)
-   }
-
-   bot.add_command(loa)
